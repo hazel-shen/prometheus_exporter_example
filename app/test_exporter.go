61285c03743f30c1d38be1a7e546a42e9954e490
hazel910159 <hazel910159@gmail.com>
2021-03-21 22:55:34 +0800

    Initial project

---
+++ app/test_exporter.go
@@ -0,0 +1,33 @@
+package main
+import (
+    "fmt"
+    "net/http"
+)
+type Counter struct{
+    count int64
+}
+
+
+
+var counter  = new(Counter)
+
+func (c *Counter) Add(count int64) int64 {
+    c.count += count
+    return c.count
+}
+
+func MetricsHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "lexporter_request_count{user=\"admin\"} %d", counter.Add(10))
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "You are the %d guests", counter.Add(10))
+
+}
+
+func main () {
+    http.HandleFunc("/metrics", MetricsHandler)
+    http.HandleFunc("/", HelloHandler)
+    http.ListenAndServe(":8000", nil)
+}
+
